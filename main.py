9bbcfefd8900627f476f65a6089bb86a1be7a718
dkrivaa <dannykrivaa@hotmail.com>
2024-08-28 08:20:27 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+# Access the parameters
+scenario_name = os.environ.get('SCENARIO_NAME')
+insurance_price = os.environ.get('INSURANCE_PRICE')
+
+print(f"Scenario Name: {scenario_name}")
+print(f"Insurance Price: {insurance_price}")
