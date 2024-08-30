8f475f2366f62addb78adfdb78b0f8ee32b70966
dkrivaa <dannykrivaa@hotmail.com>
2024-08-30 14:14:09 +0300

    initial commit

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from scenarioData import get_data
 data_dict = get_data()
 print(data_dict)
 
-df = pd.DataFrame([data_dict])
-
-df.to_excel('test.xlsx', index=False)
+# df = pd.DataFrame([data_dict])
+#
+# df.to_excel('test.xlsx', index=False)
 
 
 
