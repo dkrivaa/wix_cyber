2d94dbc64e5aaacb02c948fea8d36cc10bde4c9e
dkrivaa <dannykrivaa@hotmail.com>
2024-08-31 12:30:53 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from io import BytesIO
+import subprocess
 
 from scenarioData import get_data
 
@@ -8,6 +9,15 @@ def test():
     data_dict = get_data()
     df = pd.DataFrame([data_dict])
     df.to_excel('myfile.xlsx', index=False)
+
+    # Add the file to the staging area
+    subprocess.run(['git', 'add', 'myfile.xlsx'], check=True)
+
+    # Commit the file
+    subprocess.run(['git', 'commit', '-m', 'Add myfile.xlsx'], check=True)
+
+    # Push the changes to the repository
+    subprocess.run(['git', 'push'], check=True)
 
 
 if __name__ == '__main__':
