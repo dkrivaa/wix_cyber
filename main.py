b5654bbc42a843778390e186b01594e32840d59a
dkrivaa <dannykrivaa@hotmail.com>
2024-08-31 12:29:07 +0300

    initial commit

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from scenarioData import get_data
 
 def test():
     data_dict = get_data()
-    print('this is the result of python script: ', data_dict)
-    return data_dict
+    df = pd.DataFrame([data_dict])
+    df.to_excel('myfile.xlsx', index=False)
+
 
 if __name__ == '__main__':
     test()
