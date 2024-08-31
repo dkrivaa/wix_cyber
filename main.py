9ab1c63c55a3dbd3022b720dd37c5f793a7fd90d
dkrivaa <dannykrivaa@hotmail.com>
2024-08-31 16:39:03 +0300

    initial commit

--- main.py
+++ main.py
@@ -10,15 +10,6 @@ def test():
     df = pd.DataFrame([data_dict])
     df.to_excel('myfile.xlsx', index=False)
 
-    # Add the file to the staging area
-    subprocess.run(['git', 'add', 'myfile.xlsx'], check=True)
-
-    # Commit the file
-    subprocess.run(['git', 'commit', '-m', 'Add myfile.xlsx'], check=True)
-
-    # Push the changes to the repository
-    subprocess.run(['git', 'push'], check=True)
-
 
 if __name__ == '__main__':
     test()
