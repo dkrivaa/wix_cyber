7c68be4917d27bf131327d71cd19e688fef4695f
dkrivaa <dannykrivaa@hotmail.com>
2024-08-31 08:11:48 +0300

    initial commit

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from io import BytesIO
 
 from scenarioData import get_data
 
-data_dict = get_data()
-print(data_dict)
+
+def test():
+    data_dict = get_data()
+    print(data_dict)
+    return data_dict
 
 # df = pd.DataFrame([data_dict])
 #
 # df.to_excel('test.xlsx', index=False)
 
 
-
+if __name__ == '__main__':
+    test()
 
 
 
