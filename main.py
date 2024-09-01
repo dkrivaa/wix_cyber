0dcf9208983e1190c1fe3785942e4bf6db528e32
dkrivaa <dannykrivaa@hotmail.com>
2024-09-01 11:45:39 +0300

    initial commit

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from scenarioData import get_data
 
 def test():
     data_dict = get_data()
+    access_token = os.getenv('ACCESS_TOKEN')
+    print(access_token[0:5])
     print(json.dumps(data_dict))
 
 
