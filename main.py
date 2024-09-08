644016e76d27a982e89cb65a89236509edd9d7c7
dkrivaa <dannykrivaa@hotmail.com>
2024-09-08 10:49:43 +0300

    initial commit

--- main.py
+++ main.py
@@ -64,9 +64,9 @@ def format_excel_file(file_path):
         col_letter = get_column_letter(col)
         prev_letter = get_column_letter(col - 1)
         ws[f'{col_letter}{last_row}'] = ws[f'{prev_letter}{last_row}'].value + ws[f'{col_letter}{last_row-1}'].value
-        ws[f'{col_letter}{last_row}'].number_format = '#,###'
-        ws[f'{col_letter}{last_row}'].font = header_font
-        ws[f'{col_letter}{last_row}'].font = PatternFill(start_color="273f5c", end_color="273f5c", fill_type="solid")
+        # ws[f'{col_letter}{last_row}'].number_format = '#,###'
+        # ws[f'{col_letter}{last_row}'].font = header_font
+        # ws[f'{col_letter}{last_row}'].font = PatternFill(start_color="273f5c", end_color="273f5c", fill_type="solid")
 
 
     # Save the formatted Excel file
