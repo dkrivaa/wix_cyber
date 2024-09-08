115358e9e3c144122594698e338c31ca842e489a
dkrivaa <dannykrivaa@hotmail.com>
2024-09-08 07:49:56 +0300

    initial commit

--- simulation.py
+++ simulation.py
@@ -157,10 +157,12 @@ def run_simulation(data_dict):
             yield env.timeout(1 / customers)
 
     def renewal(env, business, customer):
-        if 0 <= math.floor(env.now) - math.floor(customer.buy_time) <= 2 and customer.package != 0:
 
-            while True:
-                yield env.timeout(12)
+        while True:
+            yield env.timeout(12)
+            if math.floor(env.now) - math.floor(customer.buy_time) > 13 or customer.package == 0:
+                break
+            else:
                 print(customer.customer_id, math.floor(customer.buy_time), math.floor(env.now))
                 customer.customer_type = 'existing'
                 customer.customer_risk = data_dict['existingRisk'] / 100
@@ -168,8 +170,7 @@ def run_simulation(data_dict):
                 customer.customer_commission = data_dict['existingCommission'] / 100
                 customer.renewal = True
                 env.process(serve_customer(env, business, customer))
-        else:
-            customer.package = 0
+
 
 
 
