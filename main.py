284d7cd60038e9e84a71ea27a1ee591dbe5d3be2
dkrivaa <dannykrivaa@hotmail.com>
2024-09-09 15:27:09 +0300

    initial commit

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def test():
         subprocess.run(['git', 'push'], check=True)
 
     # Wait for a few seconds to ensure the deletions are processed
-    time.sleep(5)
+    time.sleep(10)
 
     # Get simulation data and run simulation
     data_dict = get_data()
